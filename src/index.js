096d3ab5f0a575be971fd00965b9b929d8debdca
vanessuniq <vanessuniq@gmail.com>
2021-04-29 18:54:19 -0400

    Imported the Firebase context to the index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import FirebaseContext from './context/FirebaseContext';
 
 
 ReactDOM.render(
   <React.StrictMode>
+  <FirebaseContext.Provider value={{firebase, FieldValue}}>
     <App />
+  </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
