0b44ea0e8cca6a62d9f198e4fa7bbc87183752f6
vanessuniq <vanessuniq@gmail.com>
2021-05-02 13:35:05 -0400

    Added suspense to fallback to a loader when the page is still loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+// use suspense for the page to fall back to a loader when fetching data and promise has not resolved
+import { lazy, Suspense } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./pages/Login";
+
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login/>
-          </Route>
-        </Switch>
+        <Suspense fallback={
+          <SkeletonTheme color="#3f0f0f" highlightColor="#202020">
+            <Skeleton circle={true} height={100}/>
+          </SkeletonTheme>}>
+          <Switch>
+            <Route exact path="/">
+              <Login/>
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
     </div>
   );
