f77f4782bde5f5a302152287fa00ee46637d9b3a
vanessuniq <vanessuniq@gmail.com>
2021-05-03 12:07:27 -0400

    Defining state and actions that will constitute the interaction in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,34 @@
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import FirebaseContext from "../context/FirebaseContext";
+
 function Login(){
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+    const [credentials, setCredentials] = useState({
+        emailAdress: "",
+        password: ""
+    });
+    const [error, setError] = useState("");
+    const isInvalid = credentials.email === "" || credentials.password === "";
+
+    const handleInputChange = (event) => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        })
+    };
+    const handleLogin = (event) => {
+        event.preventDefault();
+        if (isInvalid){
+         setError("Email or password should not be empty.");
+        } else {
+         // do something else
+        };
+    };
+    useEffect(() => {
+        document.title = "Login - Instagram";
+    }, [])
     return(
         <div>
             Login Page
