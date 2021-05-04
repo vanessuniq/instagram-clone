112cbf585f40d26805bf8bc07c48b40899236866
vanessuniq <vanessuniq@gmail.com>
2021-05-03 20:51:06 -0400

    Added login form, logic and style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
+import { SIGN_UP } from "../constants/routes";
 import FirebaseContext from "../context/FirebaseContext";
 
 function Login(){
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
     const [credentials, setCredentials] = useState({
-        emailAdress: "",
+        emailAddress: "",
         password: ""
     });
     const [error, setError] = useState("");
@@ -31,8 +33,61 @@ function Login(){
     }, [])
     return(
         <div className="container flex mx-auto max-w-screen-md items-center 
-        h-screen">
-            Login Page
+            h-screen"
+        >
+           <div className="flex w-3/5">
+            <img src="/images/iphone-with-profile.jpg" 
+                alt="Iphone with instagram"
+            />
+           </div>
+
+           <div className="flex flex-col w-2/5">
+            <div className="flex flex-col bg-white justify-center
+                items-center mb-4 p-4 border border-gray-primary rounded"
+            >
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt="Instagram" 
+                        className="mt-2 w-6/12"
+                    />
+                </h1>
+
+                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+                <form onSubmit={handleLogin}>
+                    <input type="text" name="emailAddress"
+                        aria-label="Enter your email address"
+                        placeholder="Email address"
+                        className="text-sm text-gray-base w-full mr-3 mb-2 py-5 
+                        px-4 h-2 border border-gray-primary rounded"
+                        value={credentials.emailAddress}
+                        onChange={handleInputChange}
+                    />
+                    <input type="password" name="password"
+                        aria-label="Enter your password"
+                        placeholder="Password"
+                        className="text-sm text-gray-base w-full mr-3 mb-2 
+                        py-5 px-4 h-2 border border-gray-primary rounded"
+                        value={credentials.password}
+                        onChange={handleInputChange}
+                    />
+                    <button disabled={isInvalid} type="submit"
+                        className={`text-white bg-blue-medium w-full rounded h-8 
+                        font-bold ${isInvalid && "opacity-50"}`}
+                    >
+                        Log in
+                    </button>
+                </form>
+           </div>
+           <div className="flex flex-col justify-center items-center
+            w-full bg-white p-4 border border-gray-primary rounded"
+           >
+            <p className="text-sm">Dont have an account?{` `}
+                <Link to={SIGN_UP} className="font-bold text-blue-medium">
+                    Sign up
+                </Link>
+            </p>
+           </div>
+         </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 module.exports = {
     future: {
-        // removeDeprecatedGapUtilities: true,
-        // purgeLayersByDefault: true,
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
     },
     purge: [],
     theme: {
         extend: {},
+        colors: {
+            white: "#ffffff",
+            blue: {
+                medium: "#005c98"
+            },
+            black: {
+                light: "#005c98",
+                faded: "#00000059"
+            },
+            gray: {
+                base: "#616161",
+                background: "#fafafa",
+                primary: "#dbdbdb"
+            },
+            red: {
+                primary: "#ed4956"
+            }
+        }
     },
     variants: {},
     plugins: [],
